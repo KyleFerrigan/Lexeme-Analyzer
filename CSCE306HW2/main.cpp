267c096b77c817ac4f36a35d0ba10df1f3c00260
kyleferrigan <Kyle.Ferrigan@iCloud.com>
2020-09-21 11:39:48 -0500

    Initial Commit

---
+++ CSCE306HW2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CSCE306HW2
+//
+//  Created by Kyle Ferrigan on 9/21/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
