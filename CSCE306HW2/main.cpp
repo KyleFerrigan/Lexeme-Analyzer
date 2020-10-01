2eca42adcb7edb0550f24e34ee5baa666ef38ae2
kyleferrigan <Kyle.Ferrigan@iCloud.com>
2020-10-01 15:55:44 -0500

    Working final prototype

--- CSCE306HW2/main.cpp
+++ CSCE306HW2/main.cpp
@@ -9,7 +9,7 @@
 #include <fstream>
 #include <map>
 #include <vector>
-#include <typeinfo>
+#include <string>
 using namespace std;
 
 
@@ -19,100 +19,117 @@ private:
     vector<string> tokens;   // source code file tokens
     map<string, string> tokenmap;  // valid lexeme/token pairs
     // other private methods
+    int curPos = 0; //Current position within the line
     
-    int curPos; //Current position within the line
-    
-    //pre:
-    //post:
+    //pre: given a line of code from the source code, uses curPos
+    //post: returns single "word"
     string phraseFind(string lineIn){ //finds a phrase based off within lineIn starting at curPos.
         string curPhrase = ""; //reset curPhrase
-        if (curPos < lineIn.size()){ //checking to make sure its not end of line
+        if (curPos < lineIn.length()-1){ //checking to make sure its not end of line
             //INTEGERS
             if(lineIn.at(curPos) >= 48 && lineIn.at(curPos) <= 57){ //first thing a int delcare it as an int: 48-57
-                while ((lineIn.at(curPos) >= 48 && lineIn.at(curPos) <= 57) && (curPos < lineIn.size())){ //Keep going until not an int or end of line
-                    curPhrase += lineIn.at(curPos);
+                while ((lineIn.at(curPos) >= 48 && lineIn.at(curPos) <= 57) && (curPos < lineIn.length())){ //Keep going until not an int or end of line
+                    curPhrase.push_back(lineIn.at(curPos));
                     curPos++;
                 }
             }
             //STRINGS
             else if(lineIn.at(curPos) == 34){ //" means start of string: 34
-                cout << "in strings within phraseFind" << endl; //TODO REMOVE
-                while ((lineIn.at(curPos) != 34) && (curPos < lineIn.size())){ //Keep going until the next " or end of line
-                    curPhrase += lineIn.at(curPos);
+                while ((lineIn.at(curPos) != 34) && (curPos < lineIn.length())){ //Keep going until the next " or end of line
+                    curPhrase.push_back(lineIn.at(curPos));
                     curPos++;
                 }
                 if (curPos >= lineIn.size()){ //Error occured, no data output as phrase isnt valid
                     curPhrase = "";
+                    cout << "String error";
+                    exit(-1);
                 }
                 else{
-                    curPhrase += '"';
+                    curPhrase.push_back('"');
                 }
             }
             //SYMBOLS
             else if((lineIn.at(curPos) >= 33 && lineIn.at(curPos) <= 47 ) || (lineIn.at(curPos) >= 58 && lineIn.at(curPos) <= 64) || (lineIn.at(curPos) >= 91 && lineIn.at(curPos) <= 96) || (lineIn.at(curPos) >= 123 && lineIn.at(curPos) <= 126)){ //symbols: 33-47 58-64 91-96 123-126
-                cout << "in symbols within phraseFind" << endl; //TODO REMOVE
-                while(((lineIn.at(curPos) >= 33 && lineIn.at(curPos) <= 47 ) || (lineIn.at(curPos) >= 58 && lineIn.at(curPos) <= 64) || (lineIn.at(curPos) >= 91 && lineIn.at(curPos) <= 96) || (lineIn.at(curPos) >= 123 && lineIn.at(curPos) <= 126)) && ((curPos < lineIn.size()))){ //Keep going until you reach anything other than another symbol or end of line
-                        curPhrase += lineIn.at(curPos);
+                while(((lineIn.at(curPos) >= 33 && lineIn.at(curPos) <= 47 ) || (lineIn.at(curPos) >= 58 && lineIn.at(curPos) <= 64) || (lineIn.at(curPos) >= 91 && lineIn.at(curPos) <= 96) || (lineIn.at(curPos) >= 123 && lineIn.at(curPos) <= 126)) && ((curPos < lineIn.length()))){ //Keep going until you reach anything other than another symbol or end of line
+                        curPhrase.push_back(lineIn.at(curPos));
                         curPos++;
                     }
             }
             //LETTERS
-            else if(lineIn.at(curPos) == ' '){//letters : 65-90, 97-122
-                cout << "in letter within phraseFind" << endl; //TODO REMOVE
+            else if((lineIn.at(curPos) >= 65 && lineIn.at(curPos) <= 90) || (lineIn.at(curPos) >= 97 && lineIn.at(curPos) <= 122)){//letters : 65-90, 97-122
                 //Keep going until a symbol or space is reached or end of line
-                while(((((lineIn.at(curPos) >= 33 && lineIn.at(curPos) <= 47 ) || (lineIn.at(curPos) >= 58 && lineIn.at(curPos) <= 64) || (lineIn.at(curPos) >= 91 && lineIn.at(curPos) <= 96) || (lineIn.at(curPos) >= 123 && lineIn.at(curPos) <= 126)) || lineIn.at(curPos) == 32) == false) && (curPos < lineIn.size())){
-                    curPhrase = lineIn.at(curPos);
+                while(((((lineIn.at(curPos) >= 33 && lineIn.at(curPos) <= 47 ) || (lineIn.at(curPos) >= 58 && lineIn.at(curPos) <= 64) || (lineIn.at(curPos) >= 91 && lineIn.at(curPos) <= 96) || (lineIn.at(curPos) >= 123 && lineIn.at(curPos) <= 126)) || (lineIn.at(curPos) == 32)) == false)  && (curPos < lineIn.length())){
+                    curPhrase.push_back(lineIn.at(curPos));
                     curPos++;
                 }
+            }
+            else if (lineIn.at(curPos) == 32){
+                curPos++;
+            }
+            else{
+                cout << "Error" << endl;
             }
         }
         return curPhrase;
     }
-    
-    //pre: must be given only a single statement, not a line
-    //post:
-    string compareToKnownPairs(string strIn){
-        //TODO compare to tokenmap then send back string that holds the token if no tokens found return NULL;
-        //getline(tokenmap)
-        return NULL;
-    }
+
 public:
     // pre: parameter refers to open data file consisting of token and lexeme pairs i.e.  s_and and t_begin begin t_int 27.  Each pair appears on its own input line.
     // post: tokenmap has been populated
-    LexAnalyzer(istream& infile){//TODO finish this
-        cout << "In LexAnalyzer" << endl; //TODO Debug Remove this
+    LexAnalyzer(istream& infile){
         //creates object and fills a tokenmap with the lexeme pairs
         //move infile to tokenmap thats all
-        //for (int i = 0; !infile.eof(); i= i+2){
-            //tokenmap.insert();
-        //}
+        string word;
+        string word2;
+        while (infile >> word) {
+            infile >> word2;
+            tokenmap.insert(pair<string, string>(word2, word));
+        }
     }
 
     // pre: 1st parameter refers to an open text file that contains source code in the language, 2nd parameter refers to an open empty output file.
     // post: If no error, the token and lexeme pairs for the given input file have been written to the output file.  If there is an error, the incomplete token/lexeme pairs, as well as an error message have written to the output file.  A success or fail message has printed to the console.
     void scanFile(istream& infile, ostream& outfile){
         //Identifies the words from the course code and outputs it to file as the lexeme pair
-        cout << "In scanFile" << endl;//TODO Debug Remove this
-    
-        //TODO while file still has data iterate to next line
-            //comparing line by line
-            string curLine;
-            getline(infile, curLine);//put current line into string buffer, iterate this to next line
-            //TODO while line still has data
-                compareToKnownPairs(phraseFind(curLine)); //TODO Append current line ends when a phrase is found
+        string curLine;
+
+        while (getline(infile, curLine)){//While file still has data iterate to next line
+            while (curPos < curLine.length()-1){//while line still has data
+                string phrase = "";
+                phrase = phraseFind(curLine);
+                try{outfile << tokenmap.at(phrase) << ", " << phrase << '\n';} //Compare to tokenmap then send back string that holds the token working?
+                catch (const std::out_of_range&){
+                    if (phrase == ""){
+
+                    }
+                    else if (phrase.at(0) >= 48 && phrase.at(0) <= 57){ //int
+                        outfile << "t_int" << ", " << phrase << '\n';
+                    }
+                    else if (phrase.at(0) == '"'){ //String
+                         outfile << "t_str" << ", " << phrase << '\n';
+                    }
+                    else{ //identifier
+                        outfile << "t_id" << ", " << phrase << '\n';
+                    }
+                }
+            }
+        }
     }
 };
 
 int main() {
-    string buffer;
+    string buffer = "";
     ifstream lexemeData("tokenlexemedata.txt");//open lemexe data file
     LexAnalyzer lex(lexemeData);//Create LexAnalyzer with lexeme data
     cout << "Input filename of Source Code: " << endl;
     cin >> buffer;
+    //buffer = "C:\\Users\\KyleF\\iCloudDrive\\Documents\\Code\\Xcode\\SoftwareDevHW\\Homework2\\CSCE306HW2\\CSCE306HW2\\sourceCode.txt";
+    //todo uncomment delete placeholder below
     ifstream sourceCode(buffer);
-    buffer = "";
     cout << "Input filename of Output File: " << endl;
     cin >> buffer;
+    //todo uncomment and delete placeholder below
+    //buffer = "C:\\Users\\KyleF\\iCloudDrive\\Documents\\Code\\Xcode\\SoftwareDevHW\\Homework2\\CSCE306HW2\\CSCE306HW2\\output.txt";
     ofstream lexOut(buffer);
     lex.scanFile(sourceCode, lexOut);//Pass source code to lexanalyzer function
     lexOut.close();//close data file
